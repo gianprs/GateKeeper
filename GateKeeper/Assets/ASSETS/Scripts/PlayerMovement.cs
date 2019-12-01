238f890c8d02a8a2f04e01934bd8c6a8b849f8d3
FrancescoDM <42539607+FraDM89@users.noreply.github.com>
2019-12-01 09:33:49 +0100

    last

--- GateKeeper/Assets/ASSETS/Scripts/PlayerMovement.cs
+++ GateKeeper/Assets/ASSETS/Scripts/PlayerMovement.cs
@@ -8,39 +8,53 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRB;
     public Animator playerAC;
 
-    public float playerSpeed = 2;
+    public float playerSpeed;
 
     Vector2 movement;
-    
+
+    float assignedSpeed;
+
+    void Start()
+    {
+        assignedSpeed = playerSpeed;
+    }
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-
-        //questo lo attivo quando ho tutte le animazioni per ogni lato
-
-        /*
-        if(movement.magnitude != 0)
+        
+        if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
         {
-            playerAC.SetFloat("_hor", movement.x);
-            playerAC.SetFloat("_ver", movement.y);
+            playerSpeed = assignedSpeed - 0.5f;
+        } 
+        else
+        {
+            playerSpeed = assignedSpeed;
         }
-        */
 
 
+        // GESTIONE ANIMATOR CONTROLLER
         if(playerAC != null)
         {
-            playerAC.SetFloat("_hor", movement.x);
-            playerAC.SetFloat("_ver", movement.y);
-        }        
+            playerAC.SetBool("move", (Input.GetButton("Horizontal") || Input.GetButton("Vertical")));
 
+            if (movement.magnitude != 0)
+            {
+                playerAC.SetFloat("Hor", movement.x);
+                playerAC.SetFloat("Ver", movement.y);
+            }
+        }        
     }
 
 
     void FixedUpdate()
     {
         if (PlayerBehaviour.instancePB.playerLife >= 1)
-        playerRB.MovePosition(playerRB.position + movement * playerSpeed * Time.fixedDeltaTime);
+        {
+            playerRB.MovePosition(playerRB.position + movement * playerSpeed * Time.fixedDeltaTime);
+        }
+        
     }
 }
