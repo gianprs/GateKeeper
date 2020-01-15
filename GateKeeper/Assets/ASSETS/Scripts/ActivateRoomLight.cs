66b73e73a4d8643be57dcb7d71d0cd0f2622993c
FrancescoDM <42539607+FraDM89@users.noreply.github.com>
2020-01-15 23:19:55 +0100

    15/01/2020 fra
    test luci

---
+++ GateKeeper/Assets/ASSETS/Scripts/ActivateRoomLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class ActivateRoomLight : MonoBehaviour
+{
+    Light2D _light;
+
+    public Collider2D myCollider;
+
+    bool activateLight;
+    
+    void Start()
+    {
+        _light = GetComponent<Light2D>();
+
+        //myCollider = GetComponentInChildren<Collider2D>();
+
+        _light.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            print("player");
+
+            _light.enabled = true;
+            
+            myCollider.enabled = false;
+
+            activateLight = true;
+        }
+    }
+}
